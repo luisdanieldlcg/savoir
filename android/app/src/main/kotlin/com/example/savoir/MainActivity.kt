aefe3759f4a94ff7500f0fc9a1ca3d65218d1b1b
Luis De La Cruz <luisdanieldlcg@gmail.com>
2024-06-13 09:15:43 -0400

    First commit

---
+++ android/app/src/main/kotlin/com/example/savoir/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.savoir
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Savoir',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Hello, World!'),
+        ),
+      ),
+    );
+  }
+}
